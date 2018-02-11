b335b95268245dec29aa992b142282085421e23c
İbrahim Ş. Örencik <o@yedincisenol.com>
2018-02-11 18:43:32 +0300

    Test cases added

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature;
+
+./vendor/bin/phpunit --version
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit;
+
+class ExampleTest extends \TestCase1
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+}

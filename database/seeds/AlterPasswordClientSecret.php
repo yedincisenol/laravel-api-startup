63f96614e77f67dce2f6d6d6915009b25452a64d
İbrahim Ş. Örencik <o@yedincisenol.com>
2018-12-17 14:37:25 +0300

    Circle CI added

---
+++ database/seeds/AlterPasswordClientSecret.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AlterPasswordClientSecret extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Laravel\Passport\Client::query()
+            ->where('id', 2)->update([
+                'secret' => 'bXKWFmfsJsQJn9iwDU2K4qEvMAb9uYvqlQqSFRiR'
+            ]);
+    }
+}
